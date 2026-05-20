5977ae73052ec7d350066ee49d243b3c298fdc3b
Pedro Costa <114515371+pdrocosta@users.noreply.github.com>
2026-05-20 14:30:32 -0300

    Update password required message in login schema

--- mapi_manager/src/schemas/loginFormSchema.jsx
+++ mapi_manager/src/schemas/loginFormSchema.jsx
@@ -2,6 +2,6 @@ import *  as Yup from 'yup';
 
 export const loginFormSchema = Yup.object().shape({
   email: Yup.string().email("Email invalido").required("Email necessario"),
-  password: Yup.string().min(6, "senha deve ter pelo menos 6 caracteres").required("senha deve ter pelo menos 6 caracteres"),
+  password: Yup.string().min(6, "senha deve ter pelo menos 6 caracteres").required("Senha obrigatoria"),
   empresa: Yup.string().required("Empresa necessaria")
 });
