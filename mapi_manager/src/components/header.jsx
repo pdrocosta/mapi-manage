a25128919a95115595005703dd97091f6d154690
Pedro Costa <114515371+pdrocosta@users.noreply.github.com>
2026-05-20 14:47:38 -0300

    Fix logout button to call logout function

--- mapi_manager/src/components/header.jsx
+++ mapi_manager/src/components/header.jsx
@@ -2,11 +2,17 @@ import { useNavigate } from "react-router";
 
 const Header = () => {
     const  navigate = useNavigate();
+    const logout = () =>{
+        
+localStorage.removeItem("userInfos");
+        localStorage.removeItem("token")
+        navigate("/login")
+
   return (
     <header>
         <h1>Mapi Manager</h1>
         <div>
-            <button onClick={navigate("/login")}>Logout</button>
+            <button onClick=() => {logout}>Logout</button>
         </div>
     </header>
     )
