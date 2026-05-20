6e5cbce5c1407c3f0ca0c7f3cea2f3f6c6f21c79
Pedro Costa <114515371+pdrocosta@users.noreply.github.com>
2026-05-20 14:29:50 -0300

    Replace placeholder elements with LoginPage and Dashboard

--- mapi_manager/src/routes/appRoutes.jsx
+++ mapi_manager/src/routes/appRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/login";
+import Dashboard from "../pages/dashboard";
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<h1>Login</h1>} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
 
   )
